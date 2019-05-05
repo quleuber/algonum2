91bc2c653c5d18361d062f043b382cb254b9a939
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2019-05-05 12:03:02 -0300

    alguma coisa com sentido

--- exe4/teste.m
+++ exe4/teste.m
@@ -1,3 +1,20 @@
+graphics_toolkit gnuplot;
+
+function xs = fillcell(n, x)
+    xs = cell(1, n);
+    xs = cellfun(@(blah) x, xs, 'UniformOutput', false);
+end
+
+N = 50;
+A = 0;
+B = 1;
+
+data = {};
+leg = {};
+
+%%%%%
+
+data = {};
 
 tipo_a = 1;
 ua     = -1;
@@ -13,12 +30,13 @@ alfa_b  = [];
 beta_b  = [];
 gamma_b = [];
 
-[x, u] = pvc(0, 1, 10,
+[x, u] = pvc(A, B, N,
     tipo_a,ua,sigma_a,alfa_a,beta_a,gamma_a,
     tipo_b,ub,sigma_b,alfa_b,beta_b,gamma_b
 );
 
-u
+data = [data, {x; u;}];
+leg = [leg; num2str(1)];
 
 %%%%%%
 
@@ -37,12 +55,13 @@ beta_b  = [];
 gamma_b = [];
 
 
-[x, u] = pvc(0, 1, 10,
+[x, u] = pvc(A, B, N,
     tipo_a,ua,sigma_a,alfa_a,beta_a,gamma_a,
     tipo_b,ub,sigma_b,alfa_b,beta_b,gamma_b
 );
 
-u
+data = [data, {x; u;}];
+leg = [leg; num2str(2)];
 
 %%%
 
@@ -61,10 +80,31 @@ beta_b  = 2;
 gamma_b = -1;
 
 
-[x, u] = pvc(0, 1, 10,
+[x, u] = pvc(A, B, N,
     tipo_a,ua,sigma_a,alfa_a,beta_a,gamma_a,
     tipo_b,ub,sigma_b,alfa_b,beta_b,gamma_b
 );
 
-u
+data = [data, {x; u;}];
+leg = [leg; num2str(3)];
+
+%%%%%
+
+numData = size(data)(2);
+linStyle = '--';
+testPLot = [data; fillcell(numData, '--')];
+
+f = @(x) x^2 + x - 1;
+xs = linspace(A, B, N)';
+ys = arrayfun(f, xs);
+testPLot = [{xs; ys; '-'}, testPLot];
+leg = ["sol"; leg];
+
+%%%%%
+
+plot(testPLot{:});
+legend(leg{:});
+title(["n = ", num2str(N)]);
+grava_grafico(["teste-" num2str(N)], "png");
+
 
