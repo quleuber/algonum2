32aedab357e96f00677f123f4c59b29e46835b16
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2019-06-22 11:14:22 -0300

    ap2 e gráfico de campo vetorial

--- exe6/ap2.sh
+++ exe6/ap2.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-N=51; M=21;
-octave-cli ./bin/sys_ap2a.m $N $M
-octave-cli ./bin/gmres_ap2a.m $N $M && \
-octave-cli ./plota.m ap2a_${N}_${M} gmres
-echo
+RUNNER=""
+RUNNER="pueue add"
 
+# N=101; M=41;
 N=101; M=81;
-octave-cli ./bin/sys_ap2a.m $N $M
-octave-cli ./bin/gmres_ap2a.m $N $M && \
-octave-cli ./plota.m ap2a_${N}_${M} gmres
-echo
 
-N=51; M=21;
-octave-cli ./bin/sor2_ap2a.m $N $M && \
-octave-cli ./plota.m ap2a_${N}_${M} sor2
-echo
+# N=51; M=21;
+$RUNNER octave-cli ./bin/sys_ap2a.m $N $M
+$RUNNER octave-cli ./bin/gmres_ap2a.m $N $M && \
+$RUNNER octave-cli ./plota.m ap2a_${N}_${M} gmres
 
-N=51; M=21;
-octave-cli ./bin/sor_ap2a.m $N $M && \
-octave-cli ./plota.m ap2a_${N}_${M} sor
-echo
+# N=51; M=21;
+$RUNNER octave-cli ./bin/sor2_ap2a.m $N $M && \
+$RUNNER octave-cli ./plota.m ap2a_${N}_${M} sor2
+
+# N=51; M=21;
+$RUNNER octave-cli ./bin/sor_ap2a.m $N $M && \
+$RUNNER octave-cli ./plota.m ap2a_${N}_${M} sor
---
+++ exe6/ap2_vecs.m
@@ -0,0 +1,44 @@
+#!/usr/bin/octave
+args = argv();
+if length(args) < 2
+    printf("Precisa de 2 parâmetros\n");
+    exit(1);
+endif
+name = args{1};
+metodo = args{2};
+
+fullname = [ name "_" metodo ];
+filename = [ "saida/" fullname ];
+load(filename);
+
+kappa = 1;
+hx = (b_b - b_a) / (n - 1);
+hy = (b_d - b_c) / (m - 1);
+
+n
+m
+
+mu = reshape(u, n, m)';
+
+dxs = zeros(m-2, n-2);
+dys = zeros(m-2, n-2);
+
+size(dxs)
+size(dys)
+
+tx = linspace(b_a+hx, b_b-hx, n-2);
+ty = linspace(b_c+hy, b_d-hy, m-2);
+
+for ix = 2 : n-1
+    for iy = 2 : m-1
+        dxs(iy-1,ix-1) = ( mu(iy+1, ix  ) - mu(iy-1, ix  ) ) / (2*hx); 
+        dys(iy-1,ix-1) = ( mu(iy  , ix+1) - mu(iy  , ix-1) ) / (2*hy);
+    endfor
+endfor
+
+size(dxs)
+size(dys)
+
+quiver(tx, ty, dxs, dys);
+
+grava_grafico([ fullname "_vecf" ], "png");
